9d85df487557b51b2ab253ab7cad843f7bc1213e
lroos <306955+luizroos@users.noreply.github.com>
2021-09-19 15:05:39 -0300

    exercicio 4

--- 4-resiliencia/orderservice/src/main/java/com/example/orderservice/service/OrderService.java
+++ 4-resiliencia/orderservice/src/main/java/com/example/orderservice/service/OrderService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 import com.example.orderservice.entity.Order;
 import com.example.orderservice.entity.PaymentInfo;
 import com.example.orderservice.utils.RandomUtils;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 @Service
 public class OrderService {
@@ -13,6 +15,7 @@ public class OrderService {
 		return new Order(orderId);
 	}
 
+	@HystrixCommand(fallbackMethod = "getPaymentInfoFallback")
 	public PaymentInfo getPaymentInfo(Order order) {
 		PaymentInfo paymentInfo = null;
 		if (RandomUtils.random50PercentError() == 1) {
@@ -23,11 +26,21 @@ public class OrderService {
 		return paymentInfo;
 	}
 
+	public PaymentInfo getPaymentInfoFallback(Order order, Throwable e) {
+		return new PaymentInfo("No information available about payments : " + e.getClass().getSimpleName());
+	}
+
+	@HystrixCommand(fallbackMethod = "getTotalItemFallback", commandProperties = {
+			@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000") })
 	public int getTotalItem(Order order) {
 		if (RandomUtils.random50PercentError() == 1) {
 			RandomUtils.randomSleep();
 		}
 		return RandomUtils.randomTotalItems();
+	}
+
+	public int getTotalItemFallback(Order order) {
+		return -1;
 	}
 
 }
